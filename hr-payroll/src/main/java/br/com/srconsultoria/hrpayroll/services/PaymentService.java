0aa10400c7f5c77b1f1ead3da256ee63f829f495
Saulus Ribeiro <saulusribeiro@gmail.com>
2021-09-22 17:03:46 -0300

    RestTemplate

--- hr-payroll/src/main/java/br/com/srconsultoria/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/br/com/srconsultoria/hrpayroll/services/PaymentService.java
@@ -1,15 +1,34 @@
 package br.com.srconsultoria.hrpayroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import br.com.srconsultoria.hrpayroll.entities.Payment;
+import br.com.srconsultoria.hrpayroll.entities.Worker;
 
 @Service
 public class PaymentService {
 	
+	@Value("${hr-worker.host}")
+	private String workerHost;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
 	public Payment getPayment(Long workerId, int days) {
+		// O Map é uma Interface , e para usar é necessário uma classe concreta HashMap
+		Map<String, String> uriVariables = new HashMap<>();
 		
-		return new Payment("Bob", 200.0, days);
+		uriVariables.put("id", ""+ workerId);
+		
+		Worker worker = restTemplate.getForObject(workerHost +"/workers/{id}", Worker.class, uriVariables);
+		
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 		
 	}
 	
