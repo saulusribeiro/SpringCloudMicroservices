3bfe6853918b7a81da8228114ecd82d89086c7ee
Saulus Ribeiro <saulusribeiro@gmail.com>
2021-09-22 18:47:20 -0300

    Feign Client

--- hr-payroll/src/main/java/br/com/srconsultoria/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/br/com/srconsultoria/hrpayroll/services/PaymentService.java
@@ -6,28 +6,20 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import br.com.srconsultoria.hrpayroll.entities.Payment;
 import br.com.srconsultoria.hrpayroll.entities.Worker;
+import br.com.srconsultoria.hrpayroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 	
-	@Value("${hr-worker.host}")
-	private String workerHost;
-	
 	@Autowired
-	private RestTemplate restTemplate;
+	private WorkerFeignClient workerFeignClient;
 	
 	public Payment getPayment(Long workerId, int days) {
-		// O Map é uma Interface , e para usar é necessário uma classe concreta HashMap
-		Map<String, String> uriVariables = new HashMap<>();
-		
-		uriVariables.put("id", ""+ workerId);
-		
-		Worker worker = restTemplate.getForObject(workerHost +"/workers/{id}", Worker.class, uriVariables);
-		
+			
+		Worker worker = workerFeignClient.findById(workerId).getBody();
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 		
 	}
