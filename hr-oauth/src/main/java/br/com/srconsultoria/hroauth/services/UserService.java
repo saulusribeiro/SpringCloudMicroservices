5d6be2f66c8c46e0da448dd102a8c59c0dcc79dc
Saulus Ribeiro <saulusribeiro@gmail.com>
2021-10-02 19:05:55 -0300

    Login e geração do Token JWT

--- hr-oauth/src/main/java/br/com/srconsultoria/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/br/com/srconsultoria/hroauth/services/UserService.java
@@ -1,6 +1,9 @@
 package br.com.srconsultoria.hroauth.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import br.com.srconsultoria.hroauth.entities.User;
@@ -10,7 +13,7 @@ import br.com.srconsultoria.hroauth.feignclients.UserFeignClient;
  * */
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 	
 	@Autowired
 	private UserFeignClient userFeignClient;
@@ -20,6 +23,18 @@ public class UserService {
 		User user = userFeignClient.findByEmail(email).getBody();
 		if(user == null) {
 			throw new IllegalArgumentException("Email not found");
+		}
+		return user;
+	}
+
+	
+	// Implementação da interface do spring security
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userFeignClient.findByEmail(username).getBody();
+		if(user == null) {
+			throw new UsernameNotFoundException("Email not found");
 		}
 		return user;
 	}
